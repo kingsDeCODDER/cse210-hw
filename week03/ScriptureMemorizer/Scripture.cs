aefc6fec09570ad929f3b999475a4d14501aaf89
Eruba Kingsley <erubakingsley2@gmail.com>
2026-09-21 11:36:05 +0100

    made changes

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        _random = new Random();
+
+        string[] wordList = text.Split(
+            ' ',
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        foreach (string word in wordList)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + "\n";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result.Trim();
+    }
+
+    public void HideRandomWords(int numberOfWords)
+    {
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberOfWords && !AllWordsHidden())
+        {
+            int randomIndex = _random.Next(_words.Count);
+
+            if (!_words[randomIndex].IsHidden())
+            {
+                _words[randomIndex].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,45 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            string hiddenText = "";
+
+            foreach (char character in _text)
+            {
+                if (char.IsLetter(character))
+                {
+                    hiddenText += "_";
+                }
+                else
+                {
+                    hiddenText += character;
+                }
+            }
+
+            return hiddenText;
+        }
+
+        return _text;
+    }
+}
